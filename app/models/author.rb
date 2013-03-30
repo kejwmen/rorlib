1d0e76a2f73baa8062257be1a3e0a02d9e0e814d
Konrad Oleksiuk <konole@me.com>
2013-03-30 15:55:36 +0100

    fix author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
-  attr_accessible :description, :name, :book_ids
+  attr_accessible :description, :name
 
-  has_many :books, :through => :authorsbooks
+  has_many :authors_books
+  has_many :books, :through => :authors_books
 end
