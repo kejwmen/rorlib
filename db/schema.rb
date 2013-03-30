a0b708f1374e41de1a7f0a1e6d90316e2d33e589
Konrad Oleksiuk <konole@me.com>
2013-03-30 16:09:33 +0100

    generate new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130330123958) do
+ActiveRecord::Schema.define(:version => 20130330150334) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
@@ -37,11 +37,11 @@ ActiveRecord::Schema.define(:version => 20130330123958) do
   end
 
   create_table "books_categories", :force => true do |t|
-    t.integer  "book_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer "book_id"
+    t.integer "category_id"
   end
+
+  add_index "books_categories", ["book_id", "category_id"], :name => "index_books_categories_on_book_id_and_category_id"
 
   create_table "categories", :force => true do |t|
     t.string   "name"
